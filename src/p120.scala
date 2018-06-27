7347c91c873c0eac0779f458130584e187666341
StandbyMe <stand-by-me@mail.com>
2018-06-27 14:25:14 +0800

    638

---
+++ src/p120.scala
@@ -0,0 +1,18 @@
+object Solution {
+  def minimumTotal(triangle: List[List[Int]]): Int = {
+    def fetch(i: Int, prev: List[Int]): Int = {
+      if (i == 0) prev(0)
+      else if (i == prev.length) prev(i - 1)
+      else Math.min(prev(i - 1), prev(i))
+    }
+
+    def helper(prev: List[Int], current: List[Int]): List[Int] = {
+      current
+        .zipWithIndex
+        .map { case (v, i) => fetch(i, prev) + v }
+    }
+
+
+    triangle.reduce(helper).min
+  }
+}
