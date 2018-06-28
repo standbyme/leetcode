3e094f928dcec597339d3c416458c4b821c51f4f
StandbyMe <stand-by-me@mail.com>
2018-06-28 15:34:50 +0800

    169

---
+++ src/p169.scala
@@ -0,0 +1,12 @@
+object Solution {
+  def helper(tup: (Int, Int), v: Int): (Int, Int) = {
+    val (prev, counter) = tup
+    if (counter == 0) (v, 1)
+    else if (prev == v) (prev, counter + 1)
+    else (prev, counter - 1)
+  }
+
+  def majorityElement(nums: Array[Int]): Int = {
+    nums.foldLeft((0, 0))(helper)._1
+  }
+}
